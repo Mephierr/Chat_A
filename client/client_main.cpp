ad23d165648474ca078b4dbe1e34ae8bb15adfa3
Mephierr <error.9g@gmail.com>
2024-12-28 14:02:51 +0300

    client

--- client/client_main.cpp
+++ client/client_main.cpp
@@ -1,51 +1,48 @@
 #include <iostream>
-#include <boost/asio.hpp>
+#include <sio_client.h>
 
-using boost::asio::ip::tcp;
+using namespace std;
+using namespace sio;
 
 class Client {
 public:
-    Client(const std::string& host, const std::string& port)
-        : io_service(), socket(io_service) {
-        connect(host, port);
+    Client() {
+        client.connect("http://localhost:3000");
+
+        client.set_open_listener([this]() {
+            cout << "Connected to server!" << endl;
+        });
+
+        client.set_close_listener([this]() {
+            cout << "Disconnected from server!" << endl;
+        });
+
+        client.socket()->on("message", [&](const message::ptr& msg) {
+            if (msg->get_flag() == message::flag_string) {
+                cout << "Message from server: " << msg->get_string() << endl;
+            }
+        });
     }
 
     void sendMessage(const std::string& message) {
-        boost::asio::write(socket, boost::asio::buffer(message));
+        client.socket()->emit("message", message);
     }
 
-    std::string receiveMessage() {
-        char reply[1024];
-        size_t reply_length = boost::asio::read(socket, boost::asio::buffer(reply, message_length));
-        return std::string(reply, reply_length);
+    void waitForInput() {
+        std::string input;
+        while (true) {
+            cout << "Enter message: ";
+            getline(cin, input);
+            sendMessage(input);
+        }
     }
 
 private:
-    void connect(const std::string& host, const std::string& port) {
-        tcp::resolver resolver(io_service);
-        tcp::resolver::results_type endpoints = resolver.resolve(host, port);
-        boost::asio::connect(socket, endpoints);
-    }
-
-    boost::asio::io_service io_service;
-    tcp::socket socket;
-    const size_t message_length = 1024; // Максимальная длина сообщения
+    client client;
 };
 
 int main() {
-    try {
-        Client client("127.0.0.1", "3000");
-
-        // Отправка сообщения на сервер
-        client.sendMessage("Hello from C++ client!\n");
-
-        // Получение ответа от сервера
-        std::string reply = client.receiveMessage();
-        std::cout << "Reply from server: " << reply << std::endl;
-
-    } catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << "\n";
-    }
-
+    Client client;
+    client.waitForInput();
     return 0;
 }
