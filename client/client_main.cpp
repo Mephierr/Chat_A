55954f0a7a8c0bea6a7481583f9fc72e1b14c731
Mephierr <error.9g@gmail.com>
2024-12-28 13:13:05 +0300

    g

--- client/client_main.cpp
+++ client/client_main.cpp
@@ -1,25 +1,50 @@
 #include <iostream>
-#include <sio_client.h>
+#include <boost/asio.hpp>
 
-using namespace std;
+using boost::asio::ip::tcp;
+
+class Client {
+public:
+    Client(const std::string& host, const std::string& port)
+        : io_service(), socket(io_service) {
+        connect(host, port);
+    }
+
+    void sendMessage(const std::string& message) {
+        boost::asio::write(socket, boost::asio::buffer(message));
+    }
+
+    std::string receiveMessage() {
+        char reply[1024];
+        size_t reply_length = boost::asio::read(socket, boost::asio::buffer(reply, message_length));
+        return std::string(reply, reply_length);
+    }
+
+private:
+    void connect(const std::string& host, const std::string& port) {
+        tcp::resolver resolver(io_service);
+        tcp::resolver::results_type endpoints = resolver.resolve(host, port);
+        boost::asio::connect(socket, endpoints);
+    }
+
+    boost::asio::io_service io_service;
+    tcp::socket socket;
+    const size_t message_length = 1024; // Максимальная длина сообщения
+};
 
 int main() {
-    sio::client client;
+    try {
+        Client client("127.0.0.1", "3000");
 
-    client.connect("http://localhost:3000");
+        // Отправка сообщения на сервер
+        client.sendMessage("Hello from C++ client!\n");
 
-    client.on("connect", [&client]() {
-        cout << "Connected to server!" << endl;
-        client.emit("message", "Hello from client!");
-    });
+        // Получение ответа от сервера
+        std::string reply = client.receiveMessage();
+        std::cout << "Reply from server: " << reply << std::endl;
 
-    client.on("disconnect", []() {
-        cout << "Disconnected from server!" << endl;
-    });
-
-    // Keep the client running
-    while (true) {
-        client.sync();
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
     }
 
     return 0;
